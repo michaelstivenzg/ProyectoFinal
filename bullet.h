a91ea2ad5ac35519aa3b4efc67844748822ee506
michaelstivenzg <80233659+michaelstivenzg@users.noreply.github.com>
2021-10-22 13:14:22 -0500

    CLASE DE LAS MUNICIONES
    CLASE BULLET - OBJETO BULLETS

--- bullet.h
+++ bullet.h
@@ -1,12 +1,27 @@
 #ifndef BULLET_H
 #define BULLET_H
+#include "player.h"
 
-#include <QObject>
-
-class Bullet
+class Bullet: public QObject,public QGraphicsItem
 {
 public:
-    Bullet();
+    Bullet(float r, float x, float y);
+    void move();
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+    float getPosx() const;
+    void setPosx(float value);
+
+    float getPosy() const;
+    void setPosy(float value);
+
+private:
+    float radio;
+    float posx=0;
+    float posy=0;
+    float speedY;
+    float accelY;
 };
 
 #endif // BULLET_H
